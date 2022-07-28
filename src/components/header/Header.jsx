89ecc6a90fb6ce9fde74baece4b39489b0aa2915
Paulius B <paulspremium@gmail.com>
2022-07-29 00:46:56 +0300

    first commit after transfer from angular

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './header.css';
+
+export default function Header() {
+  return (
+    <div className="container">
+      <div>LOGO</div>
+      <div>SEARCH BAR</div>
+      <div>CITY</div>
+      <div>SERVICE</div>
+      <div>SHOP</div>
+      <div>LANGUAGE</div>
+      <div>MY ACCOUNT</div>
+    </div>
+  );
+}
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './hero.css';
+
+export default function Hero() {
+  return (
+    <div className="container">
+      <div className="description">
+        <p>hero works!</p>
+        <button>More info</button>
+      </div>
+      <div className="image"></div>
+    </div>
+  );
+}
