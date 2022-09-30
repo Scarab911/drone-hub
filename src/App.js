bc32b45560b389ea6cd132655254ed7dfc8155ac
Paulius B <paulspremium@gmail.com>
2022-09-30 06:33:19 +0300

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+//COMPONENTS
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+import Login from './components/login/Login';
 import './index.css';
 
 function App() {
   return (
     <div>
-      <Header></Header>
-      <Main />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      {/* <Main /> */}
       <Footer />
     </div>
   );
