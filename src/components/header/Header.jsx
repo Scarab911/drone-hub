630833e65d24e469104a9d9b145b66e26541a01d
Paulius B <paulspremium@gmail.com>
2022-10-04 06:26:32 +0300

    added link to login page and LOGO

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './header.css';
 import Search from './search_bar/Search';
 import Navigation from './navigation/Navigation';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className="header-container">
-      <h1>LOGO</h1>
+      <h1>
+        <Link to="/">LOGO</Link>
+      </h1>
       <Search />
       <Navigation />
     </div>
--- src/components/header/profile_select/drop_down_menu/DropDownMenu.jsx
+++ src/components/header/profile_select/drop_down_menu/DropDownMenu.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
-import DropDownItem from './dd_item/DropDownItem';
 import '../drop_down_menu/dropdown.css';
+import DropDownItem from './dd_item/DropDownItem';
+import { Link } from 'react-router-dom';
 
 export default function DropDownMenu() {
   return (
     <div className="drop-down-menu">
-      <DropDownItem>Nustatymai</DropDownItem>
-      <DropDownItem>Uzsakymai</DropDownItem>
-      <DropDownItem>Atsijungti</DropDownItem>
-      <DropDownItem>Prisijungti</DropDownItem>
+      <DropDownItem>
+        <Link to="/setttings">Nustatymai</Link>
+      </DropDownItem>
+      <DropDownItem>
+        <Link to="/orders">Užsakymai</Link>
+      </DropDownItem>
+      <DropDownItem>
+        <Link to="/">Atsijungti</Link>
+      </DropDownItem>
+      <DropDownItem>
+        <Link to="/login">Prisijungti</Link>
+      </DropDownItem>
     </div>
   );
 }
