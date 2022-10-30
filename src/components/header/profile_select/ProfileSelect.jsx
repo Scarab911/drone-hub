d1c9f1b40ece84e116f3e594b899670dbb802622
Paulius B <paulspremium@gmail.com>
2022-10-31 01:58:50 +0200

    profile drop down click bug and click away event

--- src/components/header/profile_select/ProfileSelect.jsx
+++ src/components/header/profile_select/ProfileSelect.jsx
@@ -5,27 +5,35 @@ import AccountImg from '../../../assets/icons/account.png';
 import './profileSelect.css';
 
 export default function ProfileSelect(props) {
-  //Adding dropdown close on mouse click away
-  const btnRef = useRef(); //needed to make event happen on specific btn
-
-  useEffect(() => {
-    const closeDropdown = (e) => {
-      if (e.path[0] !== btnRef.current) {
-        setOpen(false);
-      }
-    };
-    document.body.addEventListener('click', closeDropdown);
-    return () => document.body.removeEventListener('click', closeDropdown);
-  }, []);
-
   //adding dropdown on button click
   const [open, setOpen] = useState(false);
   if (open) console.log('paspausta:', open);
 
+  //Adding dropdown close on mouse click away
+  const btnRef = useRef(); //marking the place to compare was it clicked on it or not
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      //checking was it clicked on a reference
+      if (open && btnRef.current && !btnRef.current.contains(e.target)) {
+        setOpen(false);
+      }
+    };
+    //listening for click to happen
+    document.body.addEventListener('click', checkIfClickedOutside);
+    return () =>
+      document.body.removeEventListener('click', checkIfClickedOutside);
+  }, [setOpen]);
+
   return (
-    <div className="profile-select-container" onClick={() => setOpen(!open)}>
-      <img ref={btnRef} src={AccountImg} alt="Select profile dropdown" />
+    <div
+      ref={btnRef}
+      className="profile-select-container"
+      onClick={() => setOpen(!open)}
+    >
+      <img src={AccountImg} alt="Select profile dropdown" />
       <span>&or;</span>
+      {/* <div className="background"></div> */}
       {open && props.children}
     </div>
   );
