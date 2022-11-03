690771cfbdf26892a441642b742d8548ed99df78
Paulius B <paulspremium@gmail.com>
2022-11-04 01:40:08 +0200

    dinamic data and loop of data array

--- src/components/user/profile/orders_card/OrdersCard.jsx
+++ src/components/user/profile/orders_card/OrdersCard.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 import OrderItem from './order_item/OrderItem';
 
+const ordersList = [
+  {
+    id: '1',
+    imgUrl: 'lol',
+    shortDesc: 'labas',
+    provider: 'Saulius',
+    price: 10,
+    currency: 'EUR',
+    hourly: true,
+  },
+  {
+    id: '2',
+    imgUrl: 'lol2222',
+    shortDesc: 'bye bye',
+    provider: 'Petras',
+    price: 100,
+    currency: 'EUR',
+    hourly: false,
+  },
+  {
+    id: '3',
+    imgUrl: 'kukuraruieku',
+    shortDesc: 'viena karta kaime',
+    provider: 'Antanas',
+    price: 15,
+    currency: 'EUR',
+    hourly: true,
+  },
+];
+
 export default function OrdersCard() {
   return (
-    <div>
-      <OrderItem></OrderItem>
-      <OrderItem></OrderItem>
-      <OrderItem></OrderItem>
-      <OrderItem></OrderItem>
-    </div>
+    <>
+      {ordersList.map((order) => (
+        <OrderItem
+          id={order.id}
+          imgUrl={order.imgUrl}
+          shortDesc={order.shortDesc}
+          provider={order.provider}
+          price={order.price}
+          currency={order.currency}
+          hourly={order.hourly}
+          key={order.id}
+        />
+      ))}
+    </>
   );
 }
--- src/components/user/profile/orders_card/order_item/OrderItem.jsx
+++ src/components/user/profile/orders_card/order_item/OrderItem.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 import './orderItem.css';
 
-export default function OrderItem() {
-  const ordersList = [
-    {
-      id: '1',
-      imgUrl: 'lol',
-      shortDesc: 'labas',
-      provider: 'Saulius',
-      price: 10,
-      currency: 'EUR',
-      hourly: true,
-    },
-  ];
+export default function OrderItem(props) {
+  const { id, imgUrl, shortDesc, provider, price, hourly, currency } = props;
+
   return (
     <div className="order-item-container">
       <ul>
-        <li>{ordersList[0].id}</li>
-        <li>{ordersList[0].imgUrl}</li>
-        <li>{ordersList[0].shortDesc}</li>
-        <li>{ordersList[0].provider}</li>
+        <li>{`${id}.`}</li>
+        <li>{imgUrl}</li>
+        <li>{shortDesc}</li>
+        <li>{provider}</li>
         <li>
-          {ordersList[0].price} {ordersList[0].currency}
+          {price} {currency}
         </li>
-        <li>{ordersList[0].hourly ? '/val' : null}</li>
+        <li>{hourly ? '/val' : null}</li>
       </ul>
     </div>
   );
