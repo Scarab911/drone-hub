7d448726d855272e27d83a2756518a13b0a073d9
Paulius B <paulspremium@gmail.com>
2022-11-11 01:12:36 +0200

    responsive header and menu icon on mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+//css
 import './header.css';
+
+//components
 import Search from './search_bar/Search';
 import Navigation from './navigation/Navigation';
-import { Link } from 'react-router-dom';
+//images
+import HamburgerIcon from '../../assets/icons/menuIcon.png';
 
 export default function Header() {
   return (
     <div className="header-container">
-      <h1>
+      <img className="burger" src={HamburgerIcon} alt="" />
+      <h1 className="logo-placeholder">
         <Link to="/">LOGO</Link>
       </h1>
       <Search />
